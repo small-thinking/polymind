061dc6738892a54fe912441bd7035a3b450a8131
Yx Jiang <2237303+yxjiang@users.noreply.github.com>
2024-03-21 16:02:15 -0700

    Update tool.py

--- polymind/core/tool.py
+++ polymind/core/tool.py
@@ -32,7 +32,7 @@ class Param(BaseModel):
 
     @field_validator("type")
     def check_type(cls, v: str) -> str:
-        allowed_simple_types = ["str", "int", "float", "ndarray", "np.ndarray"]
+        allowed_simple_types = ["str", "int", "float", "bool", "ndarray", "np.ndarray", "pandas.DataFrame"]
         if v in allowed_simple_types:
             return v
         # Validating Dict type with specific format for key and value types
