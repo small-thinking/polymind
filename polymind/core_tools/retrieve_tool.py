9f8c5d568bc91b32c9d4858f5e3dfefe03e03c46
Yx Jiang <2237303+yxjiang@users.noreply.github.com>
2024-04-14 17:03:40 -0700

    Connect chain of tasks

--- polymind/core_tools/retrieve_tool.py
+++ polymind/core_tools/retrieve_tool.py
@@ -59,16 +59,6 @@ class KnowledgeRetrieveTool(RetrieveTool):
         ]
         return extra_params
 
-    async def _ranking(self, input: Message, response: Message) -> Message:
-        """Rank the input based on the query.
-
-        Args:
-            input (Message): The input message.
-            response (Message): The response message.
-        """
-        # TODO: Implement the ranking algorithm. For now, just return the response.
-        return response
-
     async def _retrieve(self, input: Message, query_embedding: List[float]) -> Message:
         # Convert from ndarray to list of list of float and there should be only one embedding.
         search_params = {
@@ -96,6 +86,16 @@ class KnowledgeRetrieveTool(RetrieveTool):
         )
         return response_message
 
+    async def _refine(self, input: Message, response: Message) -> Message:
+        """Refine the results based on the retrieved data and the input.
+
+        Args:
+            input (Message): The input message.
+            response (Message): The response message that includes the retrieved data.
+        """
+        # TODO: Implement the ranking algorithm. For now, just return the response.
+        return response
+
 
 class ToolRetriever(RetrieveTool):
     """ToolRetrieve is used to retrieve the learned tools from the Milvus database."""
@@ -111,17 +111,20 @@ class ToolRetriever(RetrieveTool):
     embedder: Embedder = Field(default=None, description="The embedder to generate the embedding for the descriptions.")
     embed_dim: int = Field(default=384, description="The dimension of the embedding.")
 
-    rank_tool_prompt_template: str = """
-        Please rank the tools based on the query query.
-        Please keep the json format as is, just change the order of the tools.
-
-        The query:
+    refine_retrieval_prompt_template: str = """
+        Please think step-by-step before answering the question.
+        Please follow the below rules:
+        1. First check each tool's name and their descriptions, and only pick from the list, not make up new tools.
+        2. We use RAG to retrieve the relevant tools to fulfill the query. The order of the tools may not be right.
+        3. Return the result as List[str], where each str is the name of the tool.
+        4. Please carefully review the available tools,
+            and pick ONLY ONE that is the highest chance to fulfill the below query:
         {query}
 
         The retrieved tools:
         {candidates}
 
-        Example ranked list in json format:
+        Example input:
         ```json
         [
             {{
@@ -139,6 +142,13 @@ class ToolRetriever(RetrieveTool):
                 'descriptions': ['tool_c_desc1', 'tool_c_desc2'],
                 'tool_file_name': 'file_c.py',
             }}
+        ]
+        ```
+
+        Example output based on the input:
+        ```json
+        [
+            'tool_a',
         ]
         ```
 
@@ -174,27 +184,6 @@ class ToolRetriever(RetrieveTool):
         ]
         return extra_params
 
-    async def _ranking(self, input: Message, response: Message) -> Message:
-        """Rank the input based on the query.
-
-        Args:
-            input (Message): The input message.
-            response (Message): The response message.
-        """
-        candidates = response.content.get(self.result_key)
-        ranking_prompt = self.rank_tool_prompt_template.format(
-            query=input.content.get(self.query_key), candidates=candidates
-        )
-        self._logger.debug(f"Before ranking: {candidates}")
-        ranking_result_message = await self._llm_tool(Message(content={"input": ranking_prompt}))
-        ranking_result_text = ranking_result_message.content.get("output", "")
-        ranked_text = extract_content_from_blob(text=ranking_result_text, blob_type="json")
-        self._logger.debug(f"Before format: {ranked_text}")
-        # Parse the ranked_text.
-        formatted_results = json.loads(ranked_text)
-        response_message = Message(content={self.result_key: formatted_results})
-        return response_message
-
     async def _retrieve(self, input: Message, query_embedding: List[float]) -> Message:
         search_params = {
             "collection_name": self.collection_name,
@@ -216,4 +205,28 @@ class ToolRetriever(RetrieveTool):
                 self.result_key: results,
             }
         )
+        return response_message
+
+    async def _refine(self, input: Message, response: Message) -> Message:
+        """Refine the results based on the retrieved tools and the input.
+
+        Args:
+            input (Message): The input message.
+            response (Message): The response message that includes the retrieved tools.
+        """
+        candidates = response.content.get(self.result_key)
+        ranking_prompt = self.refine_retrieval_prompt_template.format(
+            query=input.content.get(self.query_key), candidates=candidates
+        )
+        self._logger.debug(f"Before refine: {candidates}")
+        ranking_result_message = await self._llm_tool(Message(content={"input": ranking_prompt}))
+        ranking_result_text = ranking_result_message.content.get("output", "")
+        ranked_text = extract_content_from_blob(text=ranking_result_text, blob_type="json")
+        self._logger.debug(f"Before format: {ranked_text}")
+        # Parse the ranked_text.
+        try:
+            formatted_results = json.loads(ranked_text)
+        except json.JSONDecodeError:
+            self._logger.error(f"Failed to parse the ranked_text: {ranked_text}")
+        response_message = Message(content={self.result_key: formatted_results})
         return response_message
