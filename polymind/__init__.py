b8fc5bb45a1506d323662e7434da3ac0c3c3403d
Yx Jiang <2237303+yxjiang@users.noreply.github.com>
2024-06-11 22:50:23 -0700

    Rremove index_tool import

--- polymind/__init__.py
+++ polymind/__init__.py
@@ -5,7 +5,8 @@ from .core.message import Message
 from .core.task import AtomTask, BaseTask, CompositeTask, SequentialTask
 from .core.tool import BaseTool, LLMTool, DspyPipelineTool
 from .core_tools.retrieve_tool import KnowledgeRetrieveTool, ToolRetriever
-from .core_tools.index_tool import KnowledgeIndexTool, ToolIndexer
+
+# from .core_tools.index_tool import KnowledgeIndexTool, ToolIndexer
 from .core.logger import Logger
 
 # Expose the tools
